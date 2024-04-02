6d29175dc525cc7b5c466a1cd76e12d431cd3216
AlicjaDev <132465822+AlicjaDev@users.noreply.github.com>
2024-04-01 21:34:06 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+const [data, setData] = useState("");
+
+useEffect(() => {
+    fetchData();
+    }, []); 
+    
+
+const fetchData = async () => {
+try {
+const response = await fetch("http://localhost:5000/api/data"); 
+if (!response.ok) {
+throw new Error("Failed Lo felch dala");
+}
+const jsonData = await response.json(); 
+setData(jsonData.message);
+} catch (error){
+console.error("Error fetching data:", error);
+}
+};
+
+return (
+<div className="App">
+<header className="App-header">
+<h1>Microservices Frontend</h1>
+<div id="backendResponse">{data}</div> </header>
+</div>
+);
+}
 export default App;
